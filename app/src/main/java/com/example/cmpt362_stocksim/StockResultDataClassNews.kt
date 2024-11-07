b50e564f4e6fe78252cb90cac6310cd8f1c6f377
Arda Cifci <aca261@sfu.ca>
2024-11-06 22:33:28 -0800

    added more dataclasses

--- app/src/main/java/com/example/cmpt362_stocksim/StockResultDataClassNews.kt
+++ app/src/main/java/com/example/cmpt362_stocksim/StockResultDataClassNews.kt
@@ -1,13 +1,17 @@
 package com.example.cmpt362_stocksim
 
 
-data class StockResultDataClassNews (    val address: Address,
-                                         val currency_name: String,
-                                         val description: String,
-                                         val homepage_url: String,
-                                         val list_date: String,
-                                         val market_cap: Double,
-                                         val name: String,
-                                         val phone_number: String,
-                                         val total_employees: Int )
+data class StockResultDataClassNews (    val id: String,
+                                         val publisher: Publish,
+                                         val title: String,
+                                         val author: String,
+                                         val published_utc: String,
+                                         val article_url: Double,
+                                         val image_url: String,
+                                         val description: String)
 
+
+data class Publish(
+    val name: String,
+    val homepage_url: String,
+)
