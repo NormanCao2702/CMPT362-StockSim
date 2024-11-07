aa465479c4487e443cee66952c730498d2dd7136
NormanCao2702 <norman.cao2702@gmail.com>
2024-11-06 22:28:06 -0800

    add back button

--- app/src/main/java/com/example/cmpt362_stocksim/BlankActivity.kt
+++ app/src/main/java/com/example/cmpt362_stocksim/BlankActivity.kt
@@ -1,9 +1,7 @@
 package com.example.cmpt362_stocksim
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class BlankActivity : AppCompatActivity() {
@@ -11,5 +9,10 @@ class BlankActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blank)
+
+        // Handle custom back button click
+        findViewById<ImageButton>(R.id.btnBack).setOnClickListener {
+            finish()
+        }
     }
 }
