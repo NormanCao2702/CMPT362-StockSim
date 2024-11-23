f8650230d2ac55a193f5c7ffbdcc86cba348ba8e
KarmanyaKhanna <f20200311@pilani.bits-pilani.ac.in>
2024-11-23 04:11:01 -0800

    UI stuff added

---
+++ app/src/main/java/com/example/cmpt362_stocksim/MyAchievementRecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.cmpt362_stocksim
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+import com.example.cmpt362_stocksim.databinding.FragmentAchievementBinding
+
+
+class MyAchievementRecyclerViewAdapter() : RecyclerView.Adapter<MyAchievementRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentAchievementBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int = 0 //change
+
+    inner class ViewHolder(binding: FragmentAchievementBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+
+        override fun toString(): String {
+            return "" //change
+        }
+    }
+
+}
--- app/src/main/java/com/example/cmpt362_stocksim/ui/social/SocialFragment.kt
+++ app/src/main/java/com/example/cmpt362_stocksim/ui/social/SocialFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +13,8 @@ import com.example.cmpt362_stocksim.databinding.FragmentSocialBinding
 class SocialFragment: Fragment() {
 
     private var _binding: FragmentSocialBinding? = null
+    private lateinit var postTextBox: EditText
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,10 +31,24 @@ class SocialFragment: Fragment() {
         _binding = FragmentSocialBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSocial
-        socialViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        postTextBox = binding.postTextbox
+
+        binding.friendListButton.setOnClickListener{
+            //friend list button click event
         }
+
+        binding.buttonCreatePost.setOnClickListener{
+            //create post button click event
+        }
+
+        binding.buttonSendPost.setOnClickListener{
+            //send post button click event
+        }
+
+//        val textView: TextView = binding.textSocial
+//        socialViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
