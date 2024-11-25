e007049c6f4ea4eb9463170d6a481fee2c4ddd2e
KarmanyaKhanna <f20200311@pilani.bits-pilani.ac.in>
2024-11-24 21:45:43 -0800

    Friend list UI

---
+++ app/src/main/java/com/example/cmpt362_stocksim/FriendListActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cmpt362_stocksim
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FriendListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
